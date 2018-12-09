4d1c5f7377af8f6d4be10588af492c92d8919022
sprh.t <sprh.tt@gmail.com>
2018-12-09 16:23:00 +0300

    Исправления PEP8

--- projoct.py
+++ projoct.py
@@ -11,26 +11,21 @@ class Entry(QWidget):
     def initUI(self):
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('')
-
         self.name = QLabel(self)
         self.name.move(65, 30)
         self.name.setText('          Привет!               ')
         self.btn = QPushButton('', self)
         self.btn.resize(150, 70)
         self.btn.move(55, 65)
-
         self.timer = QBasicTimer()
         self.step = 0
         self.timer.start(10, self)
 
-    / Users / sofya / Desktop / Kyrsu / project
     def timerEvent(self, e):
-
         if self.step >= 50:
             print(self.step)
             self.but()
             self.timer.stop()
-
         self.step = self.step + 1
 
     def but(self):
@@ -44,6 +39,7 @@ class Entry(QWidget):
         self.ok = Main()
         self.ok.show()
         self.close()
+
 
 class Main(QWidget):
     def __init__(self):
